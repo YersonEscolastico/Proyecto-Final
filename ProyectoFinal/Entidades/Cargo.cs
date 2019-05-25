7d0a6ac09b5cad939bbad027586b1cf61bb19218
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-05-25 10:40:51 +0800

    Agregando la entidad cargo

---
+++ ProyectoFinal/Entidades/Cargo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Entidades
+{
+    class Cargo
+    {
+        [Key]
+        public int CargoId { get; set; }
+        public string Descripcion { get; set; }
+
+        public Cargo()
+        {
+            CargoId = 0;
+            Descripcion= string.Empty;
+        }
+    }
+}
